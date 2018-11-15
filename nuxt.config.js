c002560e502a7495a6bea58f1fd03005f56dff15
Sébastien Chopin <seb@chopin.io>
2018-11-15 18:29:31 -0500

    fix: Add polyfill for intersectionObserver

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,9 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Vue Toronto example of SSR & Advanced components design.' }
+    ],
+    script: [
+      { src: 'https://polyfill.io/v2/polyfill.min.js?features=IntersectionObserver', body: true }
     ]
   },
   css: [
